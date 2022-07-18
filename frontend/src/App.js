13a29864ebe801d26b347cae546a1e87e51207d8
Tom Hooper <tomwbhooper@hotmail.com>
2022-07-18 16:47:29 +1200

    install and use bootstrap for basic frontend components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,41 @@
-import './App.css';
-import axios from 'axios';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import NavBar from './components/NavBar';
+
 
 const App = () => {
   const [people, setPeople] = useState([]);
 
   useEffect(() => {
-    axios.get('/api').then(res => setPeople(res.data)).catch(err => console.log(err));
+    const fetchPeople = async () => {
+      const response = await axios.get('/api')
+
+      setPeople(response.data)
+    }
+    
+    fetchPeople()
+      .catch(err => console.log(err))
   }, [])
 
   return (
     <>
-      <h1>Hello World!</h1>
-      <ul>
-        {people.map((person, index) => {
-          return (
-            <li key={index}>{person.name} - {person.age}</li>
-          )
-        })}
-      </ul>
+      <Container>
+        <NavBar />
+        <Row>
+          <Col as='h1'>
+            Tom Hooper
+          </Col>
+        </Row>
+        <Row>
+          <Col as='h1'>
+            Software Engineer
+          </Col>  
+        </Row>   
+      </Container>
     </>
   )
 }
